e6dc8417b083383f0c26afe3c8b46907ea0b687f
khalil <khalilmedeb@gmail.com>
2022-10-18 00:06:07 +0200

    first_commit

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        Welcome to my home page
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        khallil
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        treppiuytrezeaioiytzii
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        nwxbnnbcnbncxnbcxn
+      </p>
+    </div>
+  );
+};
+
+export default Home;
